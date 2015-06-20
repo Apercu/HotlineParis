2e7d27f5a7a1270b937f771fec59ad77375ab145
Balthazar Gronon <bgronon@gmail.com>
2015-06-20 10:26:55 +0200

    o

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	private Animator legs;
+	private Rigidbody2D rbody;
+
+	public float speed = 100.0f;
+
+	void Start () {
+		legs = gameObject.transform.FindChild("legs").GetComponent<Animator>();
+		rbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+			rbody.AddForce(Vector3.up * speed);
+
+		}
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			rbody.AddForce(Vector3.left * speed);
+		}
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+			rbody.AddForce(Vector3.down * speed);
+		}
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			rbody.AddForce(Vector3.right * speed);
+		}
+	}
+}
