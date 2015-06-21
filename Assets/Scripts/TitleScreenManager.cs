0fbe1985f0a82ba9fa3b96722bf69e9f4aa4a5aa
Yann Pringault <ypringau@student.42.fr>
2015-06-21 12:48:52 +0200

    feat(core): intro

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScreenManager : MonoBehaviour {
+
+	private Animator playButtonAnimator;
+	public GameObject playButton;
+	public GameObject quitButton;
+
+	void Awake () {
+		playButtonAnimator = playButton.GetComponent<Animator> ();
+	}
+
+	void Start () {
+		StartCoroutine (PlayButton ());
+	}
+
+	IEnumerator PlayButton () {
+		yield return new WaitForSeconds (1f);
+		playButton.transform.FindChild("Text").GetComponent<Text> ().color = new Color (1, 1, 1, 1);
+		quitButton.transform.FindChild("Text").GetComponent<Text> ().color = new Color (1, 1, 1, 1);
+		playButtonAnimator.SetTrigger ("emphasize");
+	}
+
+	void Update () {
+
+	}
+
+	public void StartGame () {
+		Application.LoadLevel ("level1");
+	}
+
+	public void QuitGame () {
+		Application.Quit ();
+	}
+
+}
