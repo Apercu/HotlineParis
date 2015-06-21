1fe6fea0dfe8c143f3272573b8866f843b93994b
Balthazar Gronon <bgronon@gmail.com>
2015-06-21 12:34:37 +0200

    feat(ai): always improving

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	public Texture2D cursor;
-	public int ennemyNumber = 1;
+	public List<IaManager> ennemies;
 
 	public static GameManager instance { get; private set; }
 	
@@ -16,9 +17,13 @@ public class GameManager : MonoBehaviour {
 		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
 	}
 
-	public void killEnnemy () {
-		--ennemyNumber;
-		if (ennemyNumber == 0) {
+	public void addEnnemy (IaManager ennemy) {
+		ennemies.Add(ennemy);
+	}
+
+	public void killEnnemy (IaManager ennemy) {
+		ennemies.Remove(ennemy);
+		if (ennemies.Count == 0) {
 			// TODO GAME END
 			Debug.Log ("WIN");
 		}
--- Assets/Scripts/IA/IaManager.cs
+++ Assets/Scripts/IA/IaManager.cs
@@ -7,6 +7,13 @@ public class IaManager : MonoBehaviour {
 	public float speed = 3.0f;
 	public float viewAngle = 100.0f;
 
+	// Weapons Handle
+	public List<Weapon> weapons;
+	public float attackSpeed = 1.0f;
+	private Weapon currentWeapon;
+	private GameObject holder;
+
+	// Tracking and Positionning
 	private Vector3 initPosition;
 	private CheckPoint checkPoint;
 	private CheckPoint firstCheckPoint;
@@ -29,7 +36,8 @@ public class IaManager : MonoBehaviour {
 	private List<Vector3> paths = new List<Vector3>();
 
 	private float nextTime = 0.0f;
-	private int layerWithoutEnnemies = ~( (1 << 12) | (1 << 10) );
+	private float shootTime = 0.0f;
+	private int layerWithoutEnnemies = ~((1 << 12) | (1 << 10));
 
 	void Start () {
 
@@ -38,7 +46,11 @@ public class IaManager : MonoBehaviour {
 			firstCheckPoint = checkPoint;
 			hasCheckPoints = true;
 		}
-		
+
+		currentWeapon = weapons[Random.Range(0, weapons.Count)];
+		holder = transform.FindChild("gunHolder").gameObject;
+		holder.GetComponent<SpriteRenderer>().sprite = currentWeapon.handled;
+
 		legs = transform.FindChild("legs").GetComponent<Animator>();
 		body = GetComponent<Animator>();
 		head = transform.FindChild("head").GetComponent<Animator>();
@@ -47,6 +59,8 @@ public class IaManager : MonoBehaviour {
 
 		initPosition = transform.position;
 		isWalking = hasCheckPoints;
+
+		GameManager.instance.addEnnemy(this);
 	}
 
 	void moveTo (Vector3 targetPosition) {
@@ -70,9 +84,15 @@ public class IaManager : MonoBehaviour {
 		gameObject.transform.FindChild("head").GetComponent<SpriteRenderer>().sortingLayerName = "Background";
 		gameObject.transform.FindChild("legs").GetComponent<SpriteRenderer>().sortingLayerName = "Background";
 		gameObject.transform.FindChild("alert").GetComponent<SpriteRenderer>().sortingLayerName = "Background";
-		GameManager.instance.killEnnemy();
+		GameManager.instance.killEnnemy(this);
 		//StartCoroutine(startBlood());
 		StartCoroutine(pauseBlood());
+	}
+
+	void shoot () {
+		GameObject go = Instantiate(currentWeapon.ammo, transform.position, transform.rotation * Quaternion.Euler(0, 0, 270)) as GameObject;
+		Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), go.GetComponent<Collider2D>());
+		Destroy (go, 10);
 	}
 
 	IEnumerator startBlood () {
@@ -104,7 +124,6 @@ public class IaManager : MonoBehaviour {
 	void Update () {
 
 		if (body.GetBool("isDead")) {
-
 			return ;
 		}
 
@@ -136,6 +155,7 @@ public class IaManager : MonoBehaviour {
 				chasing = false;
 				lastTimeKnown = -1.0f;
 				isGoingToBercail = false;
+				isWalking = false;
 			}
 		}
 
@@ -151,16 +171,18 @@ public class IaManager : MonoBehaviour {
 
 			// On a retrouve le player
 			if (hit.collider.tag == "Player") {
-				moveTo(vectorPlayer);
+				if (Vector3.Distance(transform.position, vectorPlayer) > 0.5f) {
+					moveTo(vectorPlayer);
+					isWalking = true;
+				}
 				transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - vectorPlayer);
 				lastKnowPosition = vectorPlayer;
-				isWalking = true;
 				isLooking = false;
 				lastTimeKnown = -1.0f;
 				alert.color = Color.white;
 			} else {
 				// On l'a pas eu, mais on va a la derniere position pour checker
-				if (Vector3.Distance(transform.position, pos) > .1f) {
+				if (Vector3.Distance(transform.position, pos) > 1.0f) {
 					moveTo(pos);
 					transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - pos);
 					isWalking = true;
@@ -218,12 +240,17 @@ public class IaManager : MonoBehaviour {
 			isWalking = true;
 		}
 
+		if (hasPlayerInSight && !isGoingToBercail && Time.time > shootTime) {
+			shoot();
+			shootTime += attackSpeed;
+		}
+
 		legs.SetBool("isWalking", isWalking);
 		head.SetBool("isLooking", isLooking);
 	}
 
 	void OnTriggerStay2D (Collider2D obj) {
-		if (!chasing && !isGoingToBercail && obj.tag == "Player") {
+		if (!isGoingToBercail && obj.tag == "Player") {
 			hasPlayerInSight = false;
 
 			Vector3 dir = obj.transform.position - transform.position;
@@ -240,6 +267,7 @@ public class IaManager : MonoBehaviour {
 	void OnTriggerExit2D (Collider2D obj) {
 		if (obj.tag == "Player") {
 			hasPlayerInSight = false;
+			shootTime = 0.0f;
 		}
 	}
 }
